5754103bf7acc52aafca7a6b315f6714366fc4a1
guzlewski <45144232+guzlewski@users.noreply.github.com>
2020-12-27 15:54:44 +0100

    Change Identity PK

--- Integracja.Server/Startup.cs
+++ Integracja.Server/Startup.cs
@@ -32,7 +32,7 @@ namespace Integracja.Server
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
