dfd5ec5c2968e7df07c23b9d5190face988f6345
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-01-27 18:50:08 +0100

    Add basic repos

---
+++ src/Integracja.Server.Core/Repositories/IGamemodeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Integracja.Server.Core.Models.Base;
+
+namespace Integracja.Server.Core.Repositories
+{
+    interface IGamemodeRepository
+    {
+        Question Get(int id);
+        IEnumerable<Question> GetAll();
+        Question Add(Question note);
+        void Update(Question note);
+        void Delete(Question note);
+    }
+}
---
+++ src/Integracja.Server.Core/Repositories/IQuestionRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Integracja.Server.Core.Models.Base;
+
+namespace Integracja.Server.Core.Repositories
+{
+    public interface IQuestionRepository
+    {
+        Question Get(int id);
+        IEnumerable<Question> GetAll();
+        Question Add(Question note);
+        void Update(Question note);
+        void Delete(Question note);
+    }
+}
