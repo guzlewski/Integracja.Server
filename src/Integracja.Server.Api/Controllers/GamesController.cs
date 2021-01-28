b252601bf96114ec3f15db750c666d372b58b426
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-01-28 10:39:53 +0100

    Add basic games controller

---
+++ src/Integracja.Server.Api/Controllers/GamesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Integracja.Server.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    { 
+        [HttpGet("[action]/{guid}")]
+        public IActionResult Join(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("[action]/{guid}")]
+        public IActionResult Leave(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("[action]/{guid}")]
+        public IActionResult GetInfo(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
