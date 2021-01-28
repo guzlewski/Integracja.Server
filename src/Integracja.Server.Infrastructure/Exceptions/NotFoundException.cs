fd2a23adea8aaefff6cec6105cf8f11e055879e3
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-01-28 11:49:13 +0100

    Add custom exceptions

---
+++ src/Integracja.Server.Infrastructure/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Integracja.Server.Infrastructure.Exceptions
+{
+    public class NotFoundException : ApiException
+    {
+        public NotFoundException(string details = null) : base(404, details)
+        {
+
+        }
+    }
+}
