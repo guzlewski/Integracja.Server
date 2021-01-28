d761c966fc1b4662e15344fc7b3d5078526535c4
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-01-28 10:39:38 +0100

    Add basic auth controller

---
+++ src/Integracja.Server.Api/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Integracja.Server.Infrastructure.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Integracja.Server.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("[action]")]
+        public IActionResult Login(LoginDto login)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
