d10b7869a6183989a3e3b8cf027d0c59f9bd04f5
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-02-03 17:04:28 +0100

    Add gamemode dto

---
+++ src/Integracja.Server.Infrastructure/DTO/GamemodeDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Integracja.Server.Infrastructure.DTO
+{
+    public class GamemodeDto
+    {
+        public int Id { get; set; }
+        public bool IsPublic { get; set; }
+        public string Name { get; set; }
+        public int TimeForFullQuiz { get; set; }
+        public int? TimeForOneQuestion { get; set; }
+        public int? NumberOfLives { get; set; }
+        public int AuthorId { get; set; }
+        public string AuthorUsername { get; set; }
+        public int GamesCount { get; set; }
+    }
+}
