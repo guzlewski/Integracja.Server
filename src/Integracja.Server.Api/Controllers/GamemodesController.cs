af1fff48f8b7d5664b807cec77138abc8c04964e
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-02-03 17:09:28 +0100

    Add gamemode controller

---
+++ src/Integracja.Server.Api/Controllers/GamemodesController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Integracja.Server.Infrastructure.DTO;
+using Integracja.Server.Infrastructure.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Integracja.Server.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamemodesController : ControllerBase
+    {
+        private readonly IGamemodeService _gamemodeService;
+
+        public GamemodesController(IGamemodeService gamemodeService)
+        {
+            _gamemodeService = gamemodeService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IEnumerable<GamemodeDto>> GetAll()
+        {
+            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            return await _gamemodeService.GetAll(userId);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<GamemodeDto>> Get(int id)
+        {
+            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            return await _gamemodeService.Get(id, userId);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<GamemodeDto>> Add(GamemodeDto dto)
+        {
+            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var entity = await _gamemodeService.Add(dto, userId);
+
+            return Created($"api/Gamemodes/{entity.Id}", entity);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<OkResult> Update(int id, [FromBody] GamemodeDto dto)
+        {
+            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            await _gamemodeService.Update(id, dto, userId);
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<OkResult> Delete(int id)
+        {
+            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            await _gamemodeService.Delete(id, userId);
+
+            return Ok();
+        }
+    }
+}
