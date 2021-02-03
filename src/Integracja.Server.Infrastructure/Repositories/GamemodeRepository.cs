8684654ade1c7f5b7f28d55ba84d34d5a7f5b95c
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-02-03 16:49:08 +0100

    Add empty gamemode repository

---
+++ src/Integracja.Server.Infrastructure/Repositories/GamemodeRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Integracja.Server.Core.Models.Base;
+using Integracja.Server.Core.Repositories;
+
+namespace Integracja.Server.Infrastructure.Repositories
+{
+    public class GamemodeRepository : IGamemodeRepository
+    {
+        public IQueryable<Gamemode> Get(int id, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Gamemode> GetAll(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Gamemode> Add(Gamemode gamemode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(Gamemode gamemode)
+        {
+            throw new NotImplementedException();
+        }        
+
+        public Task Update(Gamemode gamemode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
