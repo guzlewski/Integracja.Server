7939b3e3ca8fbb6e94ed2ea01c5cbf40fb70c449
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-02-03 16:39:22 +0100

    Add api exception

---
+++ src/Integracja.Server.Infrastructure/Exceptions/BadRequestException.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Integracja.Server.Infrastructure.Exceptions
+{
+    public class BadRequestException : ApiException
+    {
+        public BadRequestException(string details = null) : base(StatusCodes.Status400BadRequest, details)
+        {
+
+        }
+    }
+}
