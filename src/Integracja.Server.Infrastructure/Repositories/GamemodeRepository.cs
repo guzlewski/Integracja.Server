e4870c4ef72467edeb56f9c2ca39acff0ccb65f8
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-02-03 16:59:48 +0100

    Implement gamemode repository

--- src/Integracja.Server.Infrastructure/Repositories/GamemodeRepository.cs
+++ src/Integracja.Server.Infrastructure/Repositories/GamemodeRepository.cs
@@ -1,38 +1,96 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Integracja.Server.Core.Models.Base;
 using Integracja.Server.Core.Repositories;
+using Integracja.Server.Infrastructure.Data;
+using Integracja.Server.Infrastructure.Exceptions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Integracja.Server.Infrastructure.Repositories
 {
     public class GamemodeRepository : IGamemodeRepository
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public GamemodeRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public IQueryable<Gamemode> Get(int id, int userId)
         {
-            throw new NotImplementedException();
+            var entity = _dbContext.Gamemodes
+                .AsNoTracking()
+                .Where(gm => gm.Id == id && (gm.IsPublic || gm.AuthorId == userId) && !gm.IsDeleted);
+
+            return entity;
         }
 
         public IQueryable<Gamemode> GetAll(int userId)
         {
-            throw new NotImplementedException();
+            var entities = _dbContext.Gamemodes
+                .AsNoTracking()
+                .Where(gm => (gm.IsPublic || gm.AuthorId == userId) && !gm.IsDeleted);
+
+            return entities;
         }
 
-        public Task<Gamemode> Add(Gamemode gamemode)
+        public async Task<Gamemode> Add(Gamemode gamemode)
         {
-            throw new NotImplementedException();
+            gamemode.Id = 0;
+
+            await _dbContext.AddAsync(gamemode);
+            await _dbContext.SaveChangesAsync();
+
+            return gamemode;
         }
 
-        public Task Delete(Gamemode gamemode)
+        public async Task Delete(Gamemode gamemode)
         {
-            throw new NotImplementedException();
-        }        
+            var entity = await _dbContext.Gamemodes
+                .Where(gm => gm.Id == gamemode.Id && gm.AuthorId == gamemode.AuthorId && !gm.IsDeleted)
+                .Select(gm => new
+                {
+                    Gamemode = gm,
+                    GamesCount = gm.Games.Count
+                })
+                .FirstOrDefaultAsync();
 
-        public Task Update(Gamemode gamemode)
+            if (entity == null)
+            {
+                throw new NotFoundException();
+            }
+
+            if (entity.GamesCount == 0)
+            {
+                _dbContext.Remove(entity.Gamemode);
+            }
+            else
+            {
+                entity.Gamemode.IsDeleted = true;
+            }
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task Update(Gamemode gamemode)
         {
-            throw new NotImplementedException();
+            var entity = await _dbContext.Gamemodes
+               .FirstOrDefaultAsync(gm => gm.Id == gamemode.Id && gm.AuthorId == gamemode.AuthorId && !gm.IsDeleted);
+
+            if (entity == null)
+            {
+                throw new NotFoundException();
+            }
+
+            entity.Name = gamemode.Name;
+            entity.TimeForFullQuiz = gamemode.TimeForFullQuiz;
+            entity.TimeForOneQuestion = gamemode.TimeForOneQuestion;
+            entity.NumberOfLives = gamemode.NumberOfLives;
+            entity.IsPublic = gamemode.IsPublic;
+            entity.RowVersion++;
+
+            await _dbContext.SaveChangesAsync();
         }
     }
 }
