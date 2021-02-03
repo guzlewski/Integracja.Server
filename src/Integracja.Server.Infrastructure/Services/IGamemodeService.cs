fe9e8208c6e894182017c466266478234246f5ce
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-02-03 17:04:19 +0100

    Add empty gamemode service

---
+++ src/Integracja.Server.Infrastructure/Services/IGamemodeService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Integracja.Server.Infrastructure.DTO;
+
+namespace Integracja.Server.Infrastructure.Services
+{
+    public interface IGamemodeService
+    {
+        Task<GamemodeDto> Get(int id, int userId);
+        Task<IEnumerable<GamemodeDto>> GetAll(int userId);
+        Task<GamemodeDto> Add(GamemodeDto dto, int userId);
+        Task Delete(int id, int userId);
+        Task Update(int id, GamemodeDto dto, int userId);
+    }
+}
---
+++ src/Integracja.Server.Infrastructure/Services/Implementations/GamemodeService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Integracja.Server.Infrastructure.DTO;
+
+namespace Integracja.Server.Infrastructure.Services.Implementations
+{
+    public class GamemodeService : IGamemodeService
+    {
+        public Task<GamemodeDto> Get(int id, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<GamemodeDto>> GetAll(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GamemodeDto> Add(GamemodeDto dto, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id, int userId)
+        {
+            throw new NotImplementedException();
+        }        
+
+        public Task Update(int id, GamemodeDto dto, int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
