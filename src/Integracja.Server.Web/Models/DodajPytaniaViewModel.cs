db4401045fa335ca00a7fb531f309cd42e19f2c3
miksuch <miksuch@users.noreply.github.com>
2021-02-10 19:14:27 +0100

    Add DodajPytaniaViewModel and update view

---
+++ src/Integracja.Server.Web/Models/DodajPytaniaViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Integracja.Server.Infrastructure.DTO;
+using System.Collections.Generic;
+
+namespace Integracja.Server.Web.Models
+{
+    public class DodajPytaniaViewModel
+    {
+        public IEnumerable<CategoryGet> Categories { get; set; }
+        public QuestionAdd NewQuestion { get; set; }
+        public IEnumerable<AnswerDto> NewQuestionAnswers { get; set; }
+        public CategoryAdd NewCategory { get; set; }
+    }
+}
