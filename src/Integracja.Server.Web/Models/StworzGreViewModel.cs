51f9c614f98631ea85d891bb94eb6b46375fd0fe
miksuch <miksuch@users.noreply.github.com>
2021-02-12 14:56:22 +0100

    Add prototype ViewModel for StworzGre

---
+++ src/Integracja.Server.Web/Models/StworzGreViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Integracja.Server.Infrastructure.DTO;
+using System.Collections.Generic;
+
+namespace Integracja.Server.Web.Models
+{
+    public class StworzGreViewModel
+    {
+        public IEnumerable<QuestionGetAll> Questions { get; set; }
+        public IEnumerable<QuestionGetAll> PickedQuestions { get; set; }
+        public GamemodeAdd Gamemode { get; set; }
+
+        //WIP
+    }
+}
