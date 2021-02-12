1395a5085007a37248610aa8d45f9c5426e170b4
miksuch <miksuch@users.noreply.github.com>
2021-02-12 14:57:21 +0100

    Add ApplicationController

---
+++ src/Integracja.Server.Web/Controllers/ApplicationController.cs
@@ -0,0 +1,23 @@
+﻿using Integracja.Server.Core.Models.Identity;
+using Integracja.Server.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Integracja.Server.Web.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        private ApplicationDbContext _context;
+        protected ApplicationDbContext DbContext { get => _context; }
+
+        private UserManager<User> _userManager;
+        protected UserManager<User> UserManager { get => _userManager; }
+
+        // using dependency injection
+        public ApplicationController( UserManager<User> userManager, ApplicationDbContext dbContext ) : base()
+        {
+            _context = dbContext;
+            _userManager = userManager;
+        }
+    }
+}
