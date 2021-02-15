f8beb55a42ed876f69a30f3a71ec33be6f6c9b13
miksuch <miksuchodolski@gmail.com>
2021-02-15 21:36:09 +0100

    Add scaffolding for PanelAdmina

---
+++ src/Integracja.Server.Web/Controllers/PanelAdminaController.cs
@@ -0,0 +1,23 @@
+﻿using Integracja.Server.Core.Models.Identity;
+using Integracja.Server.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Integracja.Server.Web.Controllers
+{
+    public class PanelAdminaController : ApplicationController
+    {
+        public PanelAdminaController(UserManager<User> userManager, ApplicationDbContext dbContext) : base(userManager, dbContext)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Integracja.Server.Web/Models/PanelAdminaViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Integracja.Server.Infrastructure.DTO;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace Integracja.Server.Web.Models
+{
+    public class PanelAdminaViewModel : PageModel
+    {
+        public bool ShowCategories { get; set; }
+        public bool ShowQuestions { get; set; }
+
+        public List<CategoryGetAll> Categories { get; set; }
+        public List<QuestionGetAll> Questions { get; set; }
+    }
+}
