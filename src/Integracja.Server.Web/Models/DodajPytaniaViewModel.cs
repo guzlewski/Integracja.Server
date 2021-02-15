ccd415f053db90a4bc6635fd0937ee13495adc80
miksuch <miksuchodolski@gmail.com>
2021-02-15 19:34:52 +0100

    Cleanup

--- src/Integracja.Server.Web/Models/DodajPytaniaViewModel.cs
+++ src/Integracja.Server.Web/Models/DodajPytaniaViewModel.cs
@@ -10,13 +10,15 @@ namespace Integracja.Server.Web.Models
     // potrzebne żeby zaagregować parę modeli które mamy na jednej stronie przykładowo DodajPytania gdzie potrzeba wyświetlać kategorię i będziemy dodawać pytania/odpowiedzi
     public class DodajPytaniaViewModel : PageModel
     {
-        public const int DefaultAnswerCount = 2;
+        
         public IEnumerable<CategoryGetAll> Categories { get; set; }
+        public CategoryAdd NewCategory { get; set; }
         public QuestionAdd NewQuestion { get; set; }
         public List<AnswerDto> NewQuestionAnswers { get; set; }
+
+        // dla mozliwosci rozszerzenia pól ?
+        public const int DefaultAnswerCount = 4;
         public int AnswerCount { get; set; }
-        public CategoryAdd NewCategory { get; set; }
-        public string TestString { get; set;  }
 
         public DodajPytaniaViewModel() : base()
         {
@@ -24,7 +26,6 @@ namespace Integracja.Server.Web.Models
             NewQuestion = new QuestionAdd();
             NewQuestionAnswers = new List<AnswerDto>();
             NewCategory = new CategoryAdd();
-            TestString = "teststring";
 
             AnswerCount = DefaultAnswerCount;
             for (int i = 0; i < AnswerCount; ++i)
