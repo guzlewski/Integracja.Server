e95039bc1e12d1cdce4f8c2859c4b57fe8dfb8ee
miksuch <miksuchodolski@gmail.com>
2021-02-17 15:40:53 +0100

    Add partial view and viewmodel for Question
    for better modularity

---
+++ src/Integracja.Server.Web/Models/QuestionViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Integracja.Server.Infrastructure.DTO;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace Integracja.Server.Web.Models
+{
+    public class QuestionViewModel : PageModel
+    {
+        public QuestionAdd Question { get; set; }
+        public List<AnswerDto> Answers { get; set; }
+        public string Title { get; set; }
+        public bool EditMode { get; set; }
+
+        // dla mozliwosci rozszerzenia pól ?
+        public const int DefaultAnswerCount = 4;
+        public int AnswerCount { get; set; }
+
+        public QuestionViewModel( string title, bool editMode, int answerCount = DefaultAnswerCount ) : base()
+        {
+            Title = title;
+            EditMode = editMode;
+            AnswerCount = answerCount;
+
+            Question = new QuestionAdd();
+            Answers = new List<AnswerDto>();
+            for (int i = 0; i < AnswerCount; ++i)
+                Answers.Add(new AnswerDto());
+        }
+    }
+}
