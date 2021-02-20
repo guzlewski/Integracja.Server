11b6229c2b6f0fc159801782f5d7dd293ac68a16
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-02-20 23:19:45 +0100

    Create GameUser service

---
+++ src/Integracja.Server.Infrastructure/Services/IGameUserService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Integracja.Server.Infrastructure.Services
+{
+    public interface IGameUserService
+    {
+        Task Join(Guid gameGuid, int userId);
+        Task Leave(Guid gameGuid, int userId);
+    }
+}
