30ea916d23f96aa53d33866e9969396b81d988bf
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-02-20 22:14:12 +0100

    Create game service

---
+++ src/Integracja.Server.Infrastructure/Services/IGameService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Integracja.Server.Infrastructure.DTO;
+
+namespace Integracja.Server.Infrastructure.Services
+{
+    public interface IGameService
+    {
+        Task<GameGet> Get(int id, int userId);
+        Task<IEnumerable<GameGetAll>> GetAll(int userId);
+        Task<int> Add(GameAdd dto, int userId);
+        Task Delete(int id, int userId);
+        Task<int> Update(int id, GameModify dto, int userId);
+    }
+}
