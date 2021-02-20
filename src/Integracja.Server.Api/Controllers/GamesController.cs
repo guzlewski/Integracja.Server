c6fd02c2c619bd825a584107995a3d463c97c50d
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-02-20 22:16:34 +0100

    Add logic to game controller

--- src/Integracja.Server.Api/Controllers/GamesController.cs
+++ src/Integracja.Server.Api/Controllers/GamesController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Integracja.Server.Infrastructure.DTO;
+using Integracja.Server.Infrastructure.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Integracja.Server.Api.Controllers
@@ -7,22 +11,56 @@ namespace Integracja.Server.Api.Controllers
     [ApiController]
     public class GamesController : DefaultController
     {
-        [HttpGet("[action]/{guid}")]
-        public IActionResult Join(Guid guid)
+        private readonly IGameService _gameService;
+
+        public GamesController(IGameService gameService)
         {
-            throw new NotImplementedException();
+            _gameService = gameService;
         }
 
-        [HttpGet("[action]/{guid}")]
-        public IActionResult Leave(Guid guid)
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IEnumerable<GameGetAll>> GetAll()
         {
-            throw new NotImplementedException();
+            return await _gameService.GetAll(UserId.Value);
         }
 
-        [HttpGet("[action]/{guid}")]
-        public IActionResult GetInfo(Guid guid)
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<GameGet>> Get(int id)
         {
-            throw new NotImplementedException();
+            return await _gameService.Get(id, UserId.Value);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> Add(GameAdd dto)
+        {
+            var entityId = await _gameService.Add(dto, UserId.Value);
+            return Created($"{Request.Path}/{entityId}", null);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> Update(int id, [FromBody] GameModify dto)
+        {
+            await _gameService.Update(id, dto, UserId.Value);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _gameService.Delete(id, UserId.Value);
+            return NoContent();
         }
     }
 }
