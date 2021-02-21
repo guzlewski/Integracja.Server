00fd1e3f57689d0db72b28bf90f7767b71b5debf
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-02-21 18:25:14 +0100

    Add migration

---
+++ src/Integracja.Server.Infrastructure/Data/Migrations/20210221171959_AddColumnsToGameUser.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Integracja.Server.Infrastructure.Data.Migrations
+{
+    public partial class AddColumnsToGameUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AnsweredQuestions",
+                table: "GameUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CorrectlyAnsweredQuestions",
+                table: "GameUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "IncorrectlyAnsweredQuestions",
+                table: "GameUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnsweredQuestions",
+                table: "GameUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CorrectlyAnsweredQuestions",
+                table: "GameUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IncorrectlyAnsweredQuestions",
+                table: "GameUsers");
+        }
+    }
+}
