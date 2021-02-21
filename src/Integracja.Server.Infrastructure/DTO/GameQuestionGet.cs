480415fe5ec9f48b9217fe2df1c2b41332a2e3c4
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-02-21 22:03:24 +0100

    Add dtos

---
+++ src/Integracja.Server.Infrastructure/DTO/GameQuestionGet.cs
@@ -0,0 +1,12 @@
+﻿namespace Integracja.Server.Infrastructure.DTO
+{
+    public class GameQuestionGet
+    {
+        public int Index { get; set; }
+        public float? OverridePositivePoints { get; set; }
+        public float? OverrideNegativePoints { get; set; }
+
+        public int QuestionId { get; set; }
+        public QuestionGet Question { get; set; }
+    }
+}
