d7b39916c9e05b7df525f2673f2bd2f082e42611
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-02-21 17:09:46 +0100

    Add conflict exception

---
+++ src/Integracja.Server.Infrastructure/Exceptions/ConflictException.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Integracja.Server.Infrastructure.Exceptions
+{
+    public class ConflictException : ApiException
+    {
+        public ConflictException(string details = null) : base(StatusCodes.Status409Conflict, details)
+        {
+
+        }
+    }
+}
