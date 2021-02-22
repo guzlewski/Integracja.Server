78d381f307630e27f74b7fec21ddbda22f92ca68
miksuch <miksuch@users.noreply.github.com>
2021-02-22 18:02:27 +0100

    Update DodajPytaniaViewModel.cs

--- src/Integracja.Server.Web/Models/DodajPytania/DodajPytaniaViewModel.cs
+++ src/Integracja.Server.Web/Models/DodajPytania/DodajPytaniaViewModel.cs
@@ -1,16 +1,14 @@
 ﻿using Integracja.Server.Infrastructure.DTO;
+using Integracja.Server.Web.Models.Question;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Http.Abstractions;
-using Microsoft.AspNetCore.Mvc;
-using System;
+using System.Threading.Tasks;
 
 namespace Integracja.Server.Web.Models
 {
-    // potrzebne żeby zaagregować parę modeli które mamy na jednej stronie przykładowo DodajPytania gdzie potrzeba wyświetlać kategorię i będziemy dodawać pytania/odpowiedzi
     public class DodajPytaniaViewModel : PageModel
     {
-        
         public IEnumerable<CategoryGetAll> Categories { get; set; }
         public CategoryAdd NewCategory { get; set; }
         public QuestionViewModel QuestionViewModel { get; set; }
@@ -18,8 +16,26 @@ namespace Integracja.Server.Web.Models
         public DodajPytaniaViewModel() : base()
         {
             Categories = new List<CategoryGetAll>();
-            QuestionViewModel = new QuestionViewModel("Twoje pytanie", true, "DodajPytania");
             NewCategory = new CategoryAdd();
+            QuestionViewModel = new QuestionViewModel("Twoje pytanie", true, "DodajPytania");
+        }
+
+        public static class ActionNames 
+        {
+            public const string CategoryCreate = nameof(IActions.CategoryCreate);
+            public const string CategoryRead = nameof(IActions.CategoryRead);
+            public const string SaveQuestionForm = nameof(IActions.SaveQuestionForm);
+        }
+        public interface IActions
+        {
+            Task<IActionResult> CategoryCreate(
+            [Bind(Prefix = nameof(DodajPytaniaViewModel.NewCategory))] CategoryAdd newCategory);
+
+            Task<IActionResult> SaveQuestionForm(
+            int? id,
+            [Bind(Prefix = nameof(QuestionViewModel.Question))] QuestionModel question);
+
+            Task<IActionResult> CategoryRead(int? categoryId);
         }
     }
 }
