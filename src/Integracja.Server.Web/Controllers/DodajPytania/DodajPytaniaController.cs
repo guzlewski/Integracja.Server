f2e2983fa671d1941e6e55c0d3930aeaaef539b5
miksuch <miksuch@users.noreply.github.com>
2021-02-22 18:08:46 +0100

    Update DodajPytaniaController.cs

--- src/Integracja.Server.Web/Controllers/DodajPytania/DodajPytaniaController.cs
+++ src/Integracja.Server.Web/Controllers/DodajPytania/DodajPytaniaController.cs
@@ -2,21 +2,21 @@
 using Integracja.Server.Infrastructure.Data;
 using Integracja.Server.Infrastructure.DTO;
 using Integracja.Server.Web.Models;
-using Microsoft.AspNetCore.Authorization;
+using Integracja.Server.Web.Models.Question;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Integracja.Server.Web.Controllers
 {
-    public class DodajPytaniaController : ApplicationController
+
+    public class DodajPytaniaController : ApplicationController, DodajPytaniaViewModel.IActions, QuestionViewModel.IActions
     {
         private DodajPytaniaViewModel Model { get; set; }
 
-        private string FormDataKey = "TemporaryFormData";
+        private const string TempDataQuestionModelKey = "QuestionModel";
 
         public DodajPytaniaController(UserManager<User> userManager, ApplicationDbContext dbContext) : base(userManager, dbContext)
         {
@@ -33,48 +33,114 @@ namespace Integracja.Server.Web.Controllers
         [ActionName("Category")]
         public IActionResult Category(int? id)
         {
-            if (id.HasValue)
-                Model.QuestionViewModel.QuestionForm.CategoryId = id.Value;
+            QuestionModel savedForm = TryRetrieveSavedForm();
+
+            if (savedForm != null)
+            {
+                Model.QuestionViewModel.Question = savedForm;
+            }
+
+            // gdyby nie było kategorii z zapisanej formy ?
+            if (id.HasValue && !Model.QuestionViewModel.Question.CategoryId.HasValue )
+                Model.QuestionViewModel.Question.CategoryId = id.Value;
+
             Model.Categories = CategoryService.GetAll(UserId).Result;
-            return View("~/Views/DodajPytania/Index.cshtml",Model);
+            return View("~/Views/DodajPytania/Index.cshtml", Model);
         }
 
         [HttpPost, ValidateAntiForgeryToken]
-        //[Route("DodajPytania/Index/{id?}/AnswerFieldAdd")]
-        [ActionName("AnswerFieldAdd")]
-        public IActionResult AnswerFieldAdd(
+        public async Task<IActionResult> AddAnswerField(
             int? id,
-            [Bind(Prefix = nameof(DodajPytaniaViewModel.QuestionViewModel.QuestionForm))] QuestionFormModel question )
+            [Bind(Prefix = nameof(QuestionViewModel.Question))] QuestionModel question )
         {
+            if (id.HasValue)
+                question.CategoryId = id.Value;
+
+            question.Answers.Add(new AnswerModel());
+
+            SaveQuestionForm(question);
+
+            return RedirectToAction("Index", "DodajPytania", new { id = question.CategoryId });
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> RemoveAnswerField(
+            int? id,
+            [Bind(Prefix = nameof(QuestionViewModel.Question))] QuestionModel question)
+        {
+            if (id.HasValue)
+                question.CategoryId = id.Value;
+
+            if( question.Answers.Count > 2 )
+                question.Answers.RemoveAt( question.Answers.Count-1 );
+
+            SaveQuestionForm(question);
+
+            return RedirectToAction("Index", "DodajPytania", new { id = question.CategoryId });
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> SaveQuestionForm(
+            int? id,
+            [Bind(Prefix = nameof(QuestionViewModel.Question))] QuestionModel question)
+        {
+            SaveQuestionForm(question);
+            return RedirectToAction("Index", "DodajPytania", new { id = id });
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProcessQuestionForm(
+            int? id,
+            [Bind(Prefix = nameof(QuestionViewModel.Question))] QuestionModel question)
+        {
+
+            if (id.HasValue)
+                question.CategoryId = id.Value;
+
+            var mapper = Web.Mappers.AutoMapperWebConfig.Initialize();
+
+            QuestionAdd questionAdd = mapper.Map<QuestionAdd>(question);
+
+            await QuestionService.Add(questionAdd, UserId);
 
             return RedirectToAction("Index", "DodajPytania", new { id = id });
         }
 
-        public IActionResult CategorySelect(int? categoryId)
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> CategoryCreate(
+            [Bind(Prefix = nameof(DodajPytaniaViewModel.NewCategory))] CategoryAdd newCategory)
+        {
+            int categoryId = await CategoryService.Add(newCategory, UserId);
+            return RedirectToAction("Index", "DodajPytania", new { id = categoryId });
+        }
+
+        public async Task<IActionResult> CategoryRead(int? categoryId) // receives categoryid from asp-route in form
         {
             return RedirectToAction("Index", "DodajPytania", new { id = categoryId });
         }
 
-        [HttpPost, ValidateAntiForgeryToken]
-        [Route("DodajPytania/Index/{id?}/QuestionAdd")]
-        [ActionName("QuestionAdd")]
-        public async Task<IActionResult> QuestionAdd(
-            int? id,
-            [Bind(Prefix = nameof(DodajPytaniaViewModel.QuestionViewModel.QuestionForm))] QuestionFormModel question )
+        private void SaveQuestionForm(QuestionModel form)
         {
-            // Assemble to QuestionAdd and Add
-            string test = question.QuestionContent;
-            return RedirectToAction("Index", "DodajPytania", new { id = id } );
+            string jsonString = JsonSerializer.Serialize<QuestionModel>(form);
+            TempData[TempDataQuestionModelKey] = jsonString;
         }
-
-        [HttpPost, ValidateAntiForgeryToken]
-        [Route("DodajPytania/CategoryAdd")]
-        [ActionName("CategoryAdd")]
-        public async Task<IActionResult> CategoryAdd(
-            [Bind(Prefix = nameof(DodajPytaniaViewModel.NewCategory))] CategoryAdd category)
+        private QuestionModel TryRetrieveSavedForm()
         {
-            int categoryId = await CategoryService.Add(category, UserId);
-            return RedirectToAction("Index", "DodajPytania", new { id = categoryId });
+            try
+            {
+                string jsonString = TempData[TempDataQuestionModelKey] as string;
+                if (jsonString == null)
+                    return null;
+                else return JsonSerializer.Deserialize<QuestionModel>(jsonString);
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+            finally
+            {
+                TempData.Clear();
+            }
         }
     }
 }
