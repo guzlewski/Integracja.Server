176f0dec694953058f2ac6685173b08522857b98
miksuch <miksuch@users.noreply.github.com>
2021-02-22 18:01:51 +0100

    Add Answer and Question models

---
+++ src/Integracja.Server.Web/Models/Shared/Question/QuestionModel.cs
@@ -0,0 +1,26 @@
+﻿using Integracja.Server.Core.Enums;
+using System.Collections.Generic;
+
+namespace Integracja.Server.Web.Models.Question
+{
+    /* służy reprezentacji formy w widoku i w kontrolerze */
+    public class QuestionModel
+    {
+        public string Content { get; set; }
+        public List<AnswerModel> Answers { get; set; }
+        public int PositivePoints { get; set; }
+        public int NegativePoints { get; set; }
+        public QuestionScoring Scoring { get; set; }
+        public int? CategoryId { get; set; }
+
+        public QuestionModel()
+        {
+        }
+        public QuestionModel(int answersCount)
+        {
+            Answers = new List<AnswerModel>();
+            for (int i = 0; i < answersCount; ++i)
+                Answers.Add(new AnswerModel());
+        }
+    }
+}
