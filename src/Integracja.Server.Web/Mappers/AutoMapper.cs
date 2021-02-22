b895cfd3d633c2436807f6f79cd9d226050fb300
miksuch <miksuch@users.noreply.github.com>
2021-02-22 18:08:25 +0100

    Add automapper for web

---
+++ src/Integracja.Server.Web/Mappers/AutoMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Integracja.Server.Infrastructure.DTO;
+using Integracja.Server.Web.Models.Question;
+
+namespace Integracja.Server.Web.Mappers
+{
+    public static class AutoMapperWebConfig
+    {
+        public static IMapper Initialize()
+        {
+            return new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<AnswerModel, AnswerDto>();
+                cfg.CreateMap<QuestionModel, QuestionAdd>();
+            })
+            .CreateMapper();
+        }
+    }
+}
