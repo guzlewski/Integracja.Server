19368f23c6357d3a424a34eb27351ebadc8864ed
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-02-25 12:47:29 +0100

    Implement Gamemode dtos

--- src/Integracja.Server.Infrastructure/Models/DetailGamemodeDto.cs
+++ src/Integracja.Server.Infrastructure/Models/DetailGamemodeDto.cs
@@ -2,6 +2,12 @@
 {
     public class DetailGamemodeDto
     {
-
+        public int Id { get; set; }
+        public int OwnerId { get; set; }
+        public bool IsPublic { get; set; }
+        public string Name { get; set; }
+        public int TimeForFullQuiz { get; set; }
+        public int? TimeForOneQuestion { get; set; }
+        public int? NumberOfLives { get; set; }
     }
 }
--- src/Integracja.Server.Infrastructure/Models/EditGamemodeDto.cs
+++ src/Integracja.Server.Infrastructure/Models/EditGamemodeDto.cs
@@ -1,7 +1,18 @@
-﻿namespace Integracja.Server.Infrastructure.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Integracja.Server.Infrastructure.Models
 {
     public class EditGamemodeDto
     {
-
+        [Required]
+        public bool? IsPublic { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required, Range(1, 31536000)]
+        public int? TimeForFullQuiz { get; set; }
+        [Range(0, 31536000)]
+        public int? TimeForOneQuestion { get; set; }
+        [Range(0, 100)]
+        public int? NumberOfLives { get; set; }
     }
 }
