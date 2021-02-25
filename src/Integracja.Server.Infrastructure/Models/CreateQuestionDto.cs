42621f0628b92e302089e3ec7baa883ae9684017
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-02-25 12:46:35 +0100

    Implement Question dtos

--- src/Integracja.Server.Infrastructure/Models/CreateQuestionDto.cs
+++ src/Integracja.Server.Infrastructure/Models/CreateQuestionDto.cs
@@ -1,7 +1,26 @@
-﻿namespace Integracja.Server.Infrastructure.Models
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Integracja.Server.Core.Enums;
+
+namespace Integracja.Server.Infrastructure.Models
 {
     public class CreateQuestionDto
     {
-
+        [Required]
+        public int? CategoryId { get; set; }
+        [Required]
+        public bool? IsPublic { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        [Range(0, 1000)]
+        public float? PositivePoints { get; set; }
+        [Required]
+        [Range(-1000, 0)]
+        public float? NegativePoints { get; set; }
+        [Required]
+        public QuestionScoring? QuestionScoring { get; set; }
+        [Required]
+        public ICollection<CreateAnswerDto> Answers { get; set; }
     }
 }
--- src/Integracja.Server.Infrastructure/Models/DetailQuestionDto.cs
+++ src/Integracja.Server.Infrastructure/Models/DetailQuestionDto.cs
@@ -1,7 +1,18 @@
-﻿namespace Integracja.Server.Infrastructure.Models
+﻿using System.Collections.Generic;
+using Integracja.Server.Core.Enums;
+
+namespace Integracja.Server.Infrastructure.Models
 {
     public class DetailQuestionDto
     {
-
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int OwnerId { get; set; }
+        public bool IsPublic { get; set; }
+        public string Content { get; set; }
+        public float PositivePoints { get; set; }
+        public float NegativePoints { get; set; }
+        public QuestionScoring QuestionScoring { get; set; }
+        public ICollection<DetailAnswerDto> Answers { get; set; }
     }
 }
