17e891860272d6608ac3b71abd182b6e6d0e2db4
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-02-25 12:47:17 +0100

    Implement Category profile

--- src/Integracja.Server.Infrastructure/Profiles/CategoryProfile.cs
+++ src/Integracja.Server.Infrastructure/Profiles/CategoryProfile.cs
@@ -1,4 +1,7 @@
-﻿using AutoMapper;
+﻿using System.Linq;
+using AutoMapper;
+using Integracja.Server.Core.Models.Base;
+using Integracja.Server.Infrastructure.Models;
 
 namespace Integracja.Server.Infrastructure.Profiles
 {
@@ -6,7 +9,21 @@ namespace Integracja.Server.Infrastructure.Profiles
     {
         public CategoryProfile()
         {
+            int userId = 0;
 
+            CreateMap<Category, CategoryDto>()
+                .ForMember(
+                    categoryDto => categoryDto.QuestionsCount,
+                    opt => opt.MapFrom(category => category.Questions.Where(question => question.IsPublic || question.OwnerId == userId).Count()));
+
+            CreateMap<Category, DetailCategoryDto>()
+                .ForMember(
+                    detailCategoryDto => detailCategoryDto.Questions,
+                    opt => opt.MapFrom(category => category.Questions.Where(question => question.IsPublic || question.OwnerId == userId)));
+
+            CreateMap<CreateCategoryDto, Category>();
+
+            CreateMap<EditCategoryDto, Category>();
         }
     }
 }
