a261b56dcb30962b2a9a9b511f2ca2f861a5a4e9
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-02-25 20:14:32 +0100

    Remove unneeded fields

--- src/Integracja.Server.Core/Models/Base/Answer.cs
+++ src/Integracja.Server.Core/Models/Base/Answer.cs
@@ -1,16 +1,11 @@
-using System;
 using System.Collections.Generic;
-using Integracja.Server.Core.Models.Interfaces;
 using Integracja.Server.Core.Models.Joins;
 
 namespace Integracja.Server.Core.Models.Base
 {
-    public class Answer : IEntity
+    public class Answer
     {
         public int Id { get; set; }
-        public DateTimeOffset? CreatedOn { get; set; }
-        public DateTimeOffset? UpdatedOn { get; set; }
-        public int RowVersion { get; set; }
 
         public string Content { get; set; }
         public bool IsCorrect { get; set; }
