341911288bb13cbbaed9052cebbd9f93c83d56ab
miksuch <miksuch@users.noreply.github.com>
2021-02-26 19:30:46 +0100

    Update CategoryModel

--- src/Integracja.Server.Web/Models/DodajPytania/CategoryModel.cs
+++ src/Integracja.Server.Web/Models/DodajPytania/CategoryModel.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Integracja.Server.Infrastructure.DTO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Integracja.Server.Web.Models.DodajPytania
 {
     public class CategoryModel
     {
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Podaj nazwę kategorii")]
         public string Name { get; set; }
+        public int QuestionsCount { get; set; }
+
+        public static List<CategoryModel> ToList( IEnumerable<CategoryGetAll> dtoList )
+        {
+            var mapper = Mappers.WebAutoMapper.Initialize();
+            List<CategoryModel> resultList = new List<CategoryModel>();
+            foreach (var dtoCategory in dtoList)
+                resultList.Add(mapper.Map<CategoryModel>(dtoCategory));
+            return resultList;
+        }
     }
 }
