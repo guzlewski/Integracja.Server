5bf59e753fa65f8f6f8c92622d17f83e1f70be2b
miksuch <miksuch@users.noreply.github.com>
2021-02-27 11:07:48 +0100

    Add table stub for User

--- src/Integracja.Server.Web/Models/PanelAdmina/PanelAdminaViewModel.cs
+++ src/Integracja.Server.Web/Models/PanelAdmina/PanelAdminaViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Integracja.Server.Infrastructure.DTO;
+﻿using Integracja.Server.Core.Models.Identity;
+using Integracja.Server.Infrastructure.DTO;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 
@@ -11,10 +12,10 @@ namespace Integracja.Server.Web.Models.PanelAdmina
 
         public IEnumerable<CategoryGetAll> Categories { get; set; }
         public IEnumerable<QuestionGetAll> Questions { get; set; }
+        public IEnumerable<User> Users { get; set; }
 
         public PanelAdminaViewModel() : base()
         {
-            Categories = new List<CategoryGetAll>();
         }
     }
 }
