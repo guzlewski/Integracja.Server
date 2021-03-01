0561f5553495d3699e006abe4ea74abbf672941e
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-03-01 22:05:37 +0100

    Add mapper profiles

--- src/Integracja.Server.Infrastructure/Profiles/GameProfile.cs
+++ src/Integracja.Server.Infrastructure/Profiles/GameProfile.cs
@@ -1,4 +1,8 @@
-﻿using AutoMapper;
+﻿using System.Linq;
+using AutoMapper;
+using Integracja.Server.Core.Enums;
+using Integracja.Server.Core.Models.Base;
+using Integracja.Server.Infrastructure.Models;
 
 namespace Integracja.Server.Infrastructure.Profiles
 {
@@ -6,7 +10,25 @@ namespace Integracja.Server.Infrastructure.Profiles
     {
         public GameProfile()
         {
+            CreateMap<Game, GameDto>()
+                .ForMember(
+                    gameDto => gameDto.PlayersCount,
+                    opt => opt.MapFrom(game => game.GameUsers.Where(gu => gu.GameUserState != GameUserState.Left).Count()));
 
+            CreateMap<Game, DetailGameDto>()
+                .ForMember(
+                    gameDto => gameDto.Players,
+                    opt => opt.MapFrom(game => game.GameUsers.Where(gu => gu.GameUserState != GameUserState.Left)));
+
+            CreateMap<CreateGameDto, Game>()
+                .ForMember(
+                    game => game.Questions,
+                    opt => opt.MapFrom(gameDto => gameDto.QuestionPool))
+                .ForMember(
+                    game => game.GameUsers,
+                    opt => opt.MapFrom(gameDto => gameDto.InvitedUsers));
+
+            CreateMap<EditGameDto, Game>();
         }
     }
 }
---
+++ src/Integracja.Server.Infrastructure/Profiles/GameUserProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Integracja.Server.Core.Models.Joins;
+using Integracja.Server.Infrastructure.Models;
+
+namespace Integracja.Server.Infrastructure.Profiles
+{
+    public class GameUserProfile : Profile
+    {
+        public GameUserProfile()
+        {
+            CreateMap<GameUser, GameUserDto>()
+               .ForMember(
+                   gameUserDto => gameUserDto.Username,
+                   opt => opt.MapFrom(gameUser => gameUser.User.UserName));
+
+            CreateMap<CreateGameUserDto, GameUser>();
+        }
+    }
+}
