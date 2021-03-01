710180e4271af078d0ed4f6db3c790477caebdfa
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-03-01 22:06:58 +0100

    Add GameQuestion IEqualityComparer

---
+++ src/Integracja.Server.Infrastructure/EqualityComparers/GameQuestionEqualityComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Integracja.Server.Core.Models.Joins;
+
+namespace Integracja.Server.Infrastructure.EqualityComparers
+{
+    class GameQuestionEqualityComparer : IEqualityComparer<GameQuestion>
+    {
+        public bool Equals(GameQuestion x, GameQuestion y)
+        {
+            return x.GameId == y.GameId &&
+                x.QuestionId == y.QuestionId;
+        }
+
+        public int GetHashCode([DisallowNull] GameQuestion obj)
+        {
+            return HashCode.Combine(obj.GameId, obj.QuestionId);
+        }
+    }
+}
