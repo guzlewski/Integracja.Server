5c2780a855bfeb340ec2aba8ba8693880ce092ba
miksuch <miksuch@users.noreply.github.com>
2021-03-01 13:56:44 +0100

    Rename and move PanelAdminaViewModel to folder

--- src/Integracja.Server.Web/Areas/PanelAdmina/Models/Home/HomeViewModel.cs
+++ src/Integracja.Server.Web/Areas/PanelAdmina/Models/Home/HomeViewModel.cs
@@ -3,9 +3,9 @@ using Integracja.Server.Infrastructure.DTO;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 
-namespace Integracja.Server.Web.Areas.PanelAdmina.Models
+namespace Integracja.Server.Web.Areas.PanelAdmina.Models.Home
 {
-    public class PanelAdminaViewModel : PageModel
+    public class HomeViewModel : PageModel
     {
         public bool ShowCategories { get; set; }
         public bool ShowQuestions { get; set; }
@@ -13,7 +13,7 @@ namespace Integracja.Server.Web.Areas.PanelAdmina.Models
         public IEnumerable<QuestionGetAll> Questions { get; set; }
         public IEnumerable<User> Users { get; set; }
 
-        public PanelAdminaViewModel() : base()
+        public HomeViewModel() : base()
         {
         }
     }
