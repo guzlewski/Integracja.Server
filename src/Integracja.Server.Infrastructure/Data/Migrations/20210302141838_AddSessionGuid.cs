98500b2001165c61def11b36d8ddd1a349385ad1
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-03-02 15:19:58 +0100

    Add User SessionGuid migration

---
+++ src/Integracja.Server.Infrastructure/Data/Migrations/20210302141838_AddSessionGuid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Integracja.Server.Infrastructure.Data.Migrations
+{
+    public partial class AddSessionGuid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "SessionGuid",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SessionGuid",
+                table: "AspNetUsers");
+        }
+    }
+}
