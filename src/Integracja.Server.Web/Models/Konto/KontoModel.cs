84a0e6d4558b17d3deb4d99fa708dce5d80b31d2
Michal <297102@o365.stud.umk.pl>
2021-03-02 18:39:14 +0100

    Add an option to add profile picture

---
+++ src/Integracja.Server.Web/Models/Konto/KontoModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Integracja.Server.Web.Models.Konto
+{
+    public class KontoModel
+    {
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        [Required(ErrorMessage = "Nie wybrano żadnego pliku.")]
+        public IFormFile File { get; set; }
+
+
+    }
+
+}
---
+++ src/Integracja.Server.Web/Models/Konto/KontoViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Integracja.Server.Web.Models.Konto
+{
+    public class KontoViewModel : PageModel
+    {
+        public KontoModel NewFile { get; set; }
+        
+        public KontoViewModel() : base()
+        {
+            NewFile = new KontoModel();
+        }
+
+        public static class ActionNames
+        {
+            public const string UploadPicture = nameof(IActions.UploadPicture);
+        }
+
+        public interface IActions
+        {
+            Task<IActionResult> UploadPicture([Bind(Prefix = nameof(NewFile))] IFormFile file); 
+        }
+    }
+
+}
