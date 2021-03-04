0cd81e64f79d19820d63ea027e87710077e2d3fa
miksuch <56126138+miksuch@users.noreply.github.com>
2021-03-04 17:38:37 +0100

    Move to files and update JS

--- src/Integracja.Server.Web/wwwroot/js/site.js
+++ src/Integracja.Server.Web/wwwroot/js/site.js
@@ -1,3 +1,35 @@
 ﻿$('#sidebarCollapse').on('click touchstart touchend', function () {
     $('#sidebar, .content').toggleClass('active');
 });
+
+saveForm = function (formId, url, callback) {
+    return $.ajax({
+        type: "POST",
+        url: url,
+        data: $("#" + formId).serialize(),
+        success: callback
+    });
+}
+
+submitForm = function (formId) {
+    $("#" + formId).submit();
+}
+
+saveFormThenSubmitForm = function (formToSaveId, saveUrl, formToSubmitId) {
+    saveForm(formToSaveId, saveUrl, submitForm(formToSubmitId));
+}
+
+saveFormThenSubmitForm2 = function () {
+    var formToSaveId = this.dataset.saveForm;
+    var saveUrl = this.dataset.saveUrl;
+    var formToSubmitId = this.dataset.submitForm;
+    saveForm(formToSaveId, saveUrl, submitForm(formToSubmitId));
+}
+
+disableFormInputs = function (formId) {
+    $("#" + formId + " :input").attr("disabled", true);
+}
+
+emptyFunction = function () {
+
+}
