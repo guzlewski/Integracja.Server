0f7572e5fa9ad5ded64026b356f451cdce46dd85
miksuch <56126138+miksuch@users.noreply.github.com>
2021-03-05 12:30:26 +0100

    Refactor in Models\Shared\Question and view update

---
+++ src/Integracja.Server.Web/Models/Shared/Question/IQuestionPartialActions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Integracja.Server.Web.Models.Shared.Question
+{
+    interface IQuestionPartialActions
+    {
+        public const string NameOfQuestionCreate = nameof(IQuestionPartialActions.QuestionCreate);
+        public const string NameOfQuestionUpdate = nameof(IQuestionPartialActions.QuestionUpdate);
+        public const string NameOfAddAnswerField = nameof(IQuestionPartialActions.AddAnswerField);
+        public const string NameOfRemoveAnswerField = nameof(IQuestionPartialActions.RemoveAnswerField);
+
+        Task<IActionResult> AddAnswerField(
+        int? categoryId,
+        QuestionModel question);
+
+        Task<IActionResult> RemoveAnswerField(
+        int? categoryId,
+        QuestionModel question);
+
+        Task<IActionResult> QuestionCreate(
+        int? categoryId,
+        QuestionModel question);
+
+        Task<IActionResult> QuestionUpdate(
+        int? categoryId,
+        QuestionModel question);
+    }
+}
---
+++ src/Integracja.Server.Web/Models/Shared/Question/QuestionPartialViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace Integracja.Server.Web.Models.Shared.Question
+{
+
+    public class QuestionPartialViewModel : PageModel
+    {
+        public string Controller { get; private set; }
+        public bool AllowEdit { get; private set; }
+        public bool AllowUpdate { get; private set; }
+
+        [BindProperty]
+        public QuestionModel Question { get; set; }
+
+        // AllowEdit miałby służyć wyświetlaniu widoku w wersji tylko do przeglądu dla użytkownika np.: dla strony przeglądania pytań
+        // nie wiem do końca jeszcze czy takie coś będzie, w jakiej postaci itd. więc na razie to martwy kod
+        public QuestionPartialViewModel( bool allowUpdate = false, bool allowEdit = true, string controllerName = "") : base()
+        {
+            Controller = controllerName;
+            AllowEdit = allowEdit;
+            if (AllowEdit == false) // jeśli nie można edytować nie można updatować
+                AllowUpdate = false;
+            else AllowUpdate = allowUpdate;
+
+            Question = new QuestionModel();
+        }
+        
+        public static class Ids
+        {
+            public const string Form = "question_form";
+        }
+    }
+}
