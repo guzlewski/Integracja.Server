a1b5048a28a2cd7df7653f1bbfc5d8fb57e52fd2
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-03-07 21:42:17 +0100

    Add QuestionsCount migration

---
+++ src/Integracja.Server.Infrastructure/Data/Migrations/20210307201836_AddQuestionsCount.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Integracja.Server.Infrastructure.Data.Migrations
+{
+    public partial class AddQuestionsCount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "QuestionsCount",
+                table: "Games",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QuestionsCount",
+                table: "Games");
+        }
+    }
+}
