fa1ada055f646ece7745525d8a60c42b657e5be9
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-03-12 21:43:17 +0100

    Add Users controller

---
+++ src/Integracja.Server.Api/Controllers/UsersController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Integracja.Server.Infrastructure.Models;
+using Integracja.Server.Infrastructure.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Integracja.Server.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : DefaultController
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IQuestionService _questionService;
+        private readonly IGamemodeService _gamemodeService;
+        private readonly IGameUserService _gameUserService;
+        private readonly IPictureService _pictureService;
+
+        public UsersController(ICategoryService categoryService, IQuestionService questionService, IGamemodeService gamemodeService, IGameUserService gameUserService, IPictureService pictureService)
+        {
+            _categoryService = categoryService;
+            _questionService = questionService;
+            _gamemodeService = gamemodeService;
+            _gameUserService = gameUserService;
+            _pictureService = pictureService;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> Games()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("[action]/{id}")]
+        public async Task<IActionResult> Games(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet("[action]")]
+        public async Task<IEnumerable<CategoryDto>> CreatedCategories()
+        {
+            return await _categoryService.GetOwned(UserId.Value);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet("[action]")]
+        public async Task<IEnumerable<QuestionDto>> CreatedQuestions()
+        {
+            return await _questionService.GetOwned(UserId.Value);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet("[action]")]
+        public async Task<IEnumerable<GamemodeDto>> CreatedGamemodes()
+        {
+            return await _gamemodeService.GetOwned(UserId.Value);
+        }
+
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status413PayloadTooLarge)]
+        [ProducesResponseType(StatusCodes.Status415UnsupportedMediaType)]
+        [HttpPost("[action]")]
+        public async Task<IActionResult> UploadPicture([Required] IFormFile profilePicture)
+        {
+            var uri = await _pictureService.Save(profilePicture, UserId.Value);
+            return Created(uri, null);
+        }
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("[action]")]
+        public async Task<IActionResult> DeletePicture()
+        {
+            await _pictureService.Delete(UserId.Value);
+            return NoContent();
+        }
+    }
+}
