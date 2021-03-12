fcd4c58dcf7a4abd852dc81cc54311298197aaf9
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-03-12 21:47:47 +0100

    Add controller helper

---
+++ src/Integracja.Server.Api/Utilities/ControllerHelper.cs
@@ -0,0 +1,13 @@
+﻿using Integracja.Server.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Integracja.Server.Api.Utilities
+{
+    public static class ControllerHelper
+    {
+        public static string GetName<T>() where T : DefaultController
+        {
+            return typeof(T).Name.Replace(nameof(Controller), string.Empty);
+        }
+    }
+}
