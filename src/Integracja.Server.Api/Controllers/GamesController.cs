bbd0e83d7a53e1bc771a7d8717a4bc4ab2ba8ae4
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-03-12 21:32:46 +0100

    Minor refactoring

--- src/Integracja.Server.Api/Controllers/GamesController.cs
+++ src/Integracja.Server.Api/Controllers/GamesController.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Integracja.Server.Api.Utilities;
 using Integracja.Server.Infrastructure.Models;
 using Integracja.Server.Infrastructure.Services.Interfaces;
 using Microsoft.AspNetCore.Http;
@@ -12,10 +14,12 @@ namespace Integracja.Server.Api.Controllers
     public class GamesController : DefaultController
     {
         private readonly IGameService _gameService;
+        private readonly IGameUserService _gameUserService;
 
-        public GamesController(IGameService gameService)
+        public GamesController(IGameService gameService, IGameUserService gameUserService)
         {
             _gameService = gameService;
+            _gameUserService = gameUserService;
         }
 
         [HttpGet]
@@ -29,7 +33,7 @@ namespace Integracja.Server.Api.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<DetailGameDto>> Get(int id)
+        public async Task<DetailGameDto> Get(int id)
         {
             return await _gameService.Get(id, UserId.Value);
         }
@@ -38,7 +42,7 @@ namespace Integracja.Server.Api.Controllers
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult> Add(CreateGameDto createGameDto)
+        public async Task<IActionResult> Add(CreateGameDto createGameDto)
         {
             var entityId = await _gameService.Add(createGameDto, UserId.Value);
             return CreatedAtAction(nameof(Get), new { id = entityId }, null);
@@ -49,7 +53,7 @@ namespace Integracja.Server.Api.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status409Conflict)]
-        public async Task<ActionResult> Update(int id, [FromBody] EditGameDto editGameDto)
+        public async Task<IActionResult> Update(int id, [FromBody] EditGameDto editGameDto)
         {
             await _gameService.Update(id, editGameDto, UserId.Value);
             return NoContent();
@@ -59,7 +63,7 @@ namespace Integracja.Server.Api.Controllers
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult> Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
             await _gameService.Delete(id, UserId.Value);
             return NoContent();
