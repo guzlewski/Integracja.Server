74d24ffcde304208eed5623fada82e53a5221808
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-03-14 20:16:43 +0100

    Implement new methods to service

--- src/Integracja.Server.Infrastructure/Services/Implementations/GameUserService.cs
+++ src/Integracja.Server.Infrastructure/Services/Implementations/GameUserService.cs
@@ -1,17 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Integracja.Server.Core.Enums;
 using Integracja.Server.Core.Repositories;
+using Integracja.Server.Infrastructure.Exceptions;
+using Integracja.Server.Infrastructure.Models;
 using Integracja.Server.Infrastructure.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Integracja.Server.Infrastructure.Services.Implementations
 {
     public class GameUserService : IGameUserService
     {
         private readonly IGameUserRepository _gameUserRepository;
+        private readonly IConfigurationProvider _configuration;
 
-        public GameUserService(IGameUserRepository gameUserRepository)
+        public GameUserService(IGameUserRepository gameUserRepository, IConfigurationProvider configuration)
         {
             _gameUserRepository = gameUserRepository;
+            _configuration = configuration;
+        }
+
+        public async Task<DetailGameUserDto> Get(int gameId, int userId)
+        {
+            var detailGameUserDto = await _gameUserRepository.Get(gameId, userId)
+                .Where(gu => gu.GameUserState != GameUserState.Left &&
+                    gu.Game.GameState != GameState.Deleted)
+                .ProjectTo<DetailGameUserDto>(_configuration)
+                .FirstOrDefaultAsync();
+
+            if (detailGameUserDto == null)
+            {
+                throw new NotFoundException();
+            }
+
+            return detailGameUserDto;
+        }
+
+        public async Task<IEnumerable<GameUserDto>> GetActive(int userId)
+        {
+            return await _gameUserRepository.GetAll(userId)
+                .Where(gu => gu.GameUserState != GameUserState.Left &&
+                    gu.Game.GameState != GameState.Deleted &&
+                    gu.Game.EndTime > DateTimeOffset.Now &&
+                    gu.AnsweredQuestions != gu.Game.QuestionsCount &&
+                    !gu.GameOver)
+                .ProjectTo<GameUserDto>(_configuration)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<GameUserDto>> GetArchived(int userId)
+        {
+            return await _gameUserRepository.GetAll(userId)
+                .Where(gu => gu.GameUserState != GameUserState.Left &&
+                    gu.Game.GameState != GameState.Deleted &&
+                    (gu.Game.EndTime <= DateTimeOffset.Now ||
+                    gu.AnsweredQuestions == gu.Game.QuestionsCount ||
+                    gu.GameOver))
+                .ProjectTo<GameUserDto>(_configuration)
+                .ToListAsync();
         }
 
         public async Task<int> Join(Guid gameGuid, int userId)
--- src/Integracja.Server.Infrastructure/Services/Interfaces/IGameUserService.cs
+++ src/Integracja.Server.Infrastructure/Services/Interfaces/IGameUserService.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Integracja.Server.Infrastructure.Models;
 
 namespace Integracja.Server.Infrastructure.Services.Interfaces
 {
     public interface IGameUserService
     {
+        Task<DetailGameUserDto> Get(int gameId, int userId);
+        Task<IEnumerable<GameUserDto>> GetActive(int userId);
+        Task<IEnumerable<GameUserDto>> GetArchived(int userId);
         Task<int> Join(Guid gameGuid, int userId);
         Task Leave(int gameId, int userId);
     }
