df044f224aa152176984a7dfe5c3cedd293f8114
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-03-17 22:03:04 +0100

    Add game related dtos

---
+++ src/Integracja.Server.Infrastructure/Models/GameUserQuestionDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Integracja.Server.Infrastructure.Models
+{
+    public class GameUserQuestionDto
+    {
+        public bool GameOver { get; set; }
+        public float? QuestionScore { get; set; }
+        public DateTimeOffset? QuestionDownloadTime { get; set; }
+        public DateTimeOffset? QuestionAnswerTime { get; set; }
+        public DetailQuestionDto<DetailAnswerDto> Question { get; set; }
+    }
+}
