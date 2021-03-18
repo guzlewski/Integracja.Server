657680fba7d377f650e92bdde91caee85787b8e0
miksuch <56126138+miksuch@users.noreply.github.com>
2021-03-18 11:09:55 +0100

    Add simple highlighting selected category for _CategorySelect.cshtml

--- src/Integracja.Server.Web/Areas/Kategorie/Models/Shared/CategorySelectViewModel.cs
+++ src/Integracja.Server.Web/Areas/Kategorie/Models/Shared/CategorySelectViewModel.cs
@@ -6,6 +6,7 @@ namespace Integracja.Server.Web.Areas.Kategorie.Models.Shared
     public class CategorySelectViewModel
     { 
         public List<CategoryModel> Categories { get; set; } = new List<CategoryModel>();
+        public int? CategorySelected { get; set; }
 
         public CategorySelectViewModel() : base()
         {
