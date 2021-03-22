050e74249ab60be8990ee64fce6d70c828b8cbbc
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-03-22 19:07:50 +0100

    Add new api response to show error code

---
+++ src/Integracja.Server.Infrastructure/Models/ApiError.cs
@@ -0,0 +1,11 @@
+﻿using Integracja.Server.Infrastructure.Enums;
+
+namespace Integracja.Server.Infrastructure.Models
+{
+    public class ApiError
+    {
+        public int StatusCode { get; set; }
+        public int ErrorCode { get; set; }
+        public string Message { get; set; }
+    }
+}
