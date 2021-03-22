3a5d287d67d3fc6d5f9d9ce8b628601a72bbe8b6
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-03-22 18:55:28 +0100

    Add ErrorCodes enum

---
+++ src/Integracja.Server.Infrastructure/Enums/ErrorCode.cs
@@ -0,0 +1,32 @@
+﻿namespace Integracja.Server.Infrastructure.Enums
+{
+    public enum ErrorCode
+    {
+        // GameLogicRepository.GetQuestion
+        AlreadyAnsweredAllQuestions,
+
+        // GameLogicRepository.SaveAnswers
+        AlreadyAnsweredThisQuestion,
+
+        // GameUserRepository.Join
+        AlreadyJoinedGame,
+
+        // GameLogicRepository.GetQuestion, GameLogicRepository.SaveAnswers
+        GameHasBeenCancelled,
+
+        // GameUserRepository.Join, GameUserRepository.Leave, GameLogicRepository.GetQuestion, GameLogicRepository.SaveAnswers
+        GameHasEnded,
+
+        // GameUserRepository.Join
+        GameIsFull,
+
+        // GameLogicRepository.GetQuestion, GameLogicRepository.SaveAnswers
+        GameIsOver,
+
+        // GameLogicRepository.GetQuestion, GameLogicRepository.SaveAnswers
+        GameTimeHasExpired,
+
+        // GameLogicRepository.SaveAnswers
+        QuestionTimeHasExpired
+    }
+}
