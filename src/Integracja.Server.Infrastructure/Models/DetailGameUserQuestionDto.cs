90ad983f6ed0f569e818baf86217f683b3ce1344
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-03-31 19:48:51 +0200

    Add dtos related to game history

---
+++ src/Integracja.Server.Infrastructure/Models/DetailGameUserQuestionDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Integracja.Server.Infrastructure.Models
+{
+    public class DetailGameUserQuestionDto
+    {
+        public float? QuestionScore { get; set; }
+        public bool IsAnswered { get; set; }
+        public DateTimeOffset? QuestionDownloadTime { get; set; }
+        public DateTimeOffset? QuestionAnswerTime { get; set; }
+        public DetailQuestionDto<DetailAnswerDto> Question { get; set; }
+        public IEnumerable<int> SelectedAnswers { get; set; }
+    }
+}
---
+++ src/Integracja.Server.Infrastructure/Models/GameUserScoreDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Integracja.Server.Infrastructure.Models
+{
+    public class GameUserScoreDto
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string ProfileThumbnail { get; set; }
+        public float GameScore { get; set; }
+    }
+}
