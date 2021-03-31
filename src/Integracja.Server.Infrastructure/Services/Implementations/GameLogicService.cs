39155dd13b6e6ccd14a7d72fc26518ad3c9bb66e
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-03-31 19:48:25 +0200

    Add GetHistory method

--- src/Integracja.Server.Infrastructure/Services/Implementations/GameLogicService.cs
+++ src/Integracja.Server.Infrastructure/Services/Implementations/GameLogicService.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Integracja.Server.Core.Enums;
 using Integracja.Server.Core.Repositories;
 using Integracja.Server.Infrastructure.Exceptions;
 using Integracja.Server.Infrastructure.Models;
@@ -13,12 +16,30 @@ namespace Integracja.Server.Infrastructure.Services.Implementations
     public class GameLogicService : IGameLogicService
     {
         private readonly IGameLogicRepository _gameLogicRepository;
+        private readonly IGameUserRepository _gameUserRepository;
         private readonly IConfigurationProvider _configuration;
 
-        public GameLogicService(IGameLogicRepository gameLogicRepository, IConfigurationProvider configuration)
+        public GameLogicService(IGameLogicRepository gameLogicRepository, IGameUserRepository gameUserRepository, IConfigurationProvider configuration)
         {
             _gameLogicRepository = gameLogicRepository;
+            _gameUserRepository = gameUserRepository;
             _configuration = configuration;
+        }
+
+        public async Task<T> GetHistory<T>(int gameId, int userId)
+        {
+            var dto = await _gameUserRepository.Get(gameId, userId)
+                .Where(gu => gu.Game.GameState == GameState.Normal && 
+                    gu.Game.EndTime <= DateTimeOffset.Now)
+                .ProjectTo<T>(_configuration)
+                .FirstOrDefaultAsync();
+
+            if (dto == null)
+            {
+                throw new NotFoundException();
+            }
+
+            return dto;
         }
 
         public async Task<T> GetQuestion<T>(int gameId, int userId)
--- src/Integracja.Server.Infrastructure/Services/Interfaces/IGameLogicService.cs
+++ src/Integracja.Server.Infrastructure/Services/Interfaces/IGameLogicService.cs
@@ -5,6 +5,7 @@ namespace Integracja.Server.Infrastructure.Services.Interfaces
 {
     public interface IGameLogicService
     {
+        Task<T> GetHistory<T>(int gameId, int userId);
         Task<T> GetQuestion<T>(int gameId, int userId);
         Task<T> SaveAnswers<T>(int gameId, int userId, int questionId, IEnumerable<int> answers);
     }
