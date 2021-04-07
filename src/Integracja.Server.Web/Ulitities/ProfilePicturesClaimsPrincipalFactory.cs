6cce93c7cf386162fcadc1fa25f70238c37709fe
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-04-07 14:37:42 +0200

    Remove DefaultPicture from Factory; moved to register

--- src/Integracja.Server.Web/Ulitities/ProfilePicturesClaimsPrincipalFactory.cs
+++ src/Integracja.Server.Web/Ulitities/ProfilePicturesClaimsPrincipalFactory.cs
@@ -8,8 +8,6 @@ namespace Integracja.Server.Web.Ulitities
 {
     public class ProfilePicturesClaimsPrincipalFactory : UserClaimsPrincipalFactory<User>
     {
-        private const string DefaultPicture = "https://as2.ftcdn.net/jpg/02/15/84/43/500_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg";
-
         public ProfilePicturesClaimsPrincipalFactory(UserManager<User> userManager, IOptions<IdentityOptions> optionsAccessor) : base(userManager, optionsAccessor)
         {
         }
@@ -17,8 +15,8 @@ namespace Integracja.Server.Web.Ulitities
         protected override async Task<ClaimsIdentity> GenerateClaimsAsync(User user)
         {
             var identity = await base.GenerateClaimsAsync(user);
-            identity.AddClaim(new Claim("ProfilePicture", user.ProfilePicture ?? DefaultPicture));
-            identity.AddClaim(new Claim("ProfileThumbnail", user.ProfileThumbnail ?? DefaultPicture));
+            identity.AddClaim(new Claim("ProfilePicture", user.ProfilePicture));
+            identity.AddClaim(new Claim("ProfileThumbnail", user.ProfileThumbnail));
 
             return identity;
         }
