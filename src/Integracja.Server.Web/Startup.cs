cab33cf4cf7d6e062a695a6f9148d1c0d635f337
guzlewski <45144232+guzlewski@users.noreply.github.com>
2021-04-07 10:57:04 +0200

    Move public field to top of the class

--- src/Integracja.Server.Web/Startup.cs
+++ src/Integracja.Server.Web/Startup.cs
@@ -16,12 +16,12 @@ namespace Integracja.Server.Web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
