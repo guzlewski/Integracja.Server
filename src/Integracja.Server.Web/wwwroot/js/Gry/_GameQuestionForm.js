d68c5781b3276d9db8e289d5becf2cf605dbda05
miksuch <56126138+miksuch@users.noreply.github.com>
2021-04-15 14:51:43 +0200

    Minor improvements to Gry UI/UX

---
+++ src/Integracja.Server.Web/wwwroot/js/Gry/_GameQuestionForm.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    $('.data-table-game-questions').DataTable({
+        initComplete: function () {
+            this.api().columns(1).every(function () {
+                var column = this;
+                var select = $('<select><option value=""></option></select>')
+                    .appendTo($(column.footer()).empty())
+                    .on('change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+
+                        column
+                            .search(val ? '^' + val + '$' : '', true, false)
+                            .draw();
+                    });
+
+                column.data().unique().sort().each(function (d, j) {
+                    if (column.search() === '^' + d + '$') {
+                        select.append('<option value="' + d + '" selected="selected">' + d + '</option>')
+                    } else {
+                        select.append('<option value="' + d + '">' + d + '</option>')
+                    }
+                });
+            });
+        }
+    });
+});
